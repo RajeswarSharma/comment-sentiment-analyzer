70c414e8bbbf2d1cbad77bbb515de8c409124a5e
RajeswarSharma <rjqwer47@gmail.com>
2021-10-26 11:35:47 +0530

    Removed false features

--- controller/youtube-controller.js
+++ controller/youtube-controller.js
@@ -8,12 +8,20 @@ function parseUrl(recivedUrl) {
     ab_channel: fixedUrl.query.ab_channel,
   };
 }
+function getComments(comments){
+  let result = [];
+  for(let i in comments){
+    result.push(comments[i].snippet.topLevelComment.snippet.textDisplay);
+  }
+  return result;
+}
 
 exports.genUrl = async (req,res) => {
     const result = parseUrl(req.body.link);
     const APIURLRaw = `https://youtube.googleapis.com/youtube/v3/commentThreads?part=snippet&maxResults=200&order=orderUnspecified&key=AIzaSyBwjPJaqpM9EQEtjeVvCTecEf66sBpnMPM&videoId=${result.v}`;
     await request(APIURLRaw,(err,response,body)=>{
         const comments = JSON.parse(body);
-        res.json(comments);
+        const processedComments  = getComments(comments.items);
+        res.json(processedComments);
     });  
 };
