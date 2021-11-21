7841a1902e9ec10a7f49ff39a64279675b44f85d
RajeswarSharma <rjqwer47@gmail.com>
2021-11-21 12:54:30 +0530

    Trained model

--- controller/youtube-controller.js
+++ controller/youtube-controller.js
@@ -1,12 +1,12 @@
 const url = require("url");
 const request = require("request");
 
-const sender = await request(options, res)
-  .then(function (parsedBody) {
+const sender = async (options, res) => {
+  await request(options, res).then(function (parsedBody) {
     console.log(parsedBody);
     res.send(parsedBody);
-  })
-
+  });
+};
 
 function parseUrl(recivedUrl) {
   const url_parts = url.parse(recivedUrl, true);
@@ -27,16 +27,16 @@ function getComments(comments) {
 exports.genUrl = async (req, res) => {
   const result = parseUrl(req.body.link);
   const APIURLRaw = `https://youtube.googleapis.com/youtube/v3/commentThreads?part=snippet&maxResults=200&order=orderUnspecified&key=AIzaSyBwjPJaqpM9EQEtjeVvCTecEf66sBpnMPM&videoId=${result.v}`;
-  await request(APIURLRaw, (err, response, body) => {
+  await request(APIURLRaw, async (err, response, body) => {
     const comments = JSON.parse(body);
     const processedComments = getComments(comments.items);
     //res.json(processedComments);
     const options = {
       method: "GET",
-      uri: "http://192.168.1.4:5000/",
+      uri: "http://192.168.1.4:5000/predict",
       body: processedComments,
       json: true,
     };
-    sender(options, res);
+    await sender(options, res);
   });
 };
